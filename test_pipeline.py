c0ade7305972375b699ebcd33f0a6febab28d0a7
gaojx <gaojxcs@gmail.com>
2019-03-20 13:19:17 +0800

    fix row limit test

--- test_pipeline.py
+++ test_pipeline.py
@@ -6,8 +6,8 @@ class TestPipeline(unittest.TestCase):
         from pipeline import compute_ner_for_clinical_trails
         NUM_OF_ROWS = 2
         row_generator = compute_ner_for_clinical_trails(NUM_OF_ROWS, "nct_id")
-        row_num = 1
+        row_num = 0
         for row in row_generator:
-            pprint(row)
+            # pprint(row)
             row_num += 1
         self.assertEqual(row_num, NUM_OF_ROWS)
