770bf5d6ed176ca105a8b0b81d88c686c4a996d3
gaojx <gaojxcs@gmail.com>
2019-03-20 11:03:25 +0800

    limit test case row num

--- test_pipeline.py
+++ test_pipeline.py
@@ -4,7 +4,7 @@ import unittest
 class TestPipeline(unittest.TestCase):
     def test_compute_ner_for_clinical_trails(self):
         from pipeline import compute_ner_for_clinical_trails
-        NUM_OF_ROWS = 10
+        NUM_OF_ROWS = 2
         row_generator = compute_ner_for_clinical_trails(NUM_OF_ROWS, "nct_id")
         row_num = 1
         for row in row_generator:
