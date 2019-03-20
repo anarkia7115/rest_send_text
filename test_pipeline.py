5705902de960f52ab673405559f5b409fb02b8ae
gaojx <gaojxcs@gmail.com>
2019-03-20 10:59:13 +0800

    add test case

---
+++ test_pipeline.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+import unittest
+
+class TestPipeline(unittest.TestCase):
+    def test_compute_ner_for_clinical_trails(self):
+        from pipeline import compute_ner_for_clinical_trails
+        NUM_OF_ROWS = 10
+        row_generator = compute_ner_for_clinical_trails(NUM_OF_ROWS, "nct_id")
+        row_num = 1
+        for row in row_generator:
+            pprint(row)
+            row_num += 1
+        self.assertEqual(row_num, NUM_OF_ROWS)
