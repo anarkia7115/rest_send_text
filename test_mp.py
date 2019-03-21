72793493a3e6c36b0d4098c0a0970d2c5dbf74b1
gaojx <gaojxcs@gmail.com>
2019-03-21 15:50:38 +0800

    add mp ner test

--- test_mp.py
+++ test_mp.py
@@ -3,6 +3,7 @@ import multiprocessing as mp
 from multiprocessing import Pool, Queue
 import time
 from functools import partial
+import pipeline
 
 STOP_SIGNAL="KILL"
 
@@ -27,11 +28,7 @@ class TestMultiProcess(unittest.TestCase):
             f.flush()
         f.close()
     
-    def test_mp_write_to_one_file(self):
-        import multiprocessing, logging
-        mpl = multiprocessing.log_to_stderr()
-        mpl.setLevel(multiprocessing.SUBDEBUG)
-
+    def test_mp_compute_ner(self):
         # initialize
         process_num = 6
 
@@ -42,20 +39,40 @@ class TestMultiProcess(unittest.TestCase):
         # start up listener (file writer)
         print("starting listener")
         output_file = "./data/test_mp_write1"
-        from pipeline import get_from_queue_and_write
-        watcher = p.apply_async(
-            self.dummy_listener, 
+        import pipeline
+        watcher = p.apply_async(pipeline.get_from_queue_and_write, 
+            args=(q, output_file, STOP_SIGNAL))
+
+        # start up worker (put data into queue)
+        print("starting worker")
+        nrows = 100
+        key_column_name = 'nct_id'
+        pipeline.load_and_compute_ner_multi_process(nrows, key_column_name, p, q)
+
+        # merge
+        print("job finished!")
+        print("sending end signal")
+        q.put(STOP_SIGNAL)
+        p.close()
+        p.join()
+
+    def test_mp_write_to_one_file(self):
+        # initialize
+        process_num = 6
+
+        manager = mp.Manager()
+        q = manager.Queue()
+        p = Pool(process_num)
+
+        # start up listener (file writer)
+        print("starting listener")
+        output_file = "./data/test_mp_write1"
+        watcher = p.apply_async(pipeline.get_from_queue_and_write, 
             args=(q, output_file))
 
         # start up worker (put data into queue)
         print("starting worker")
-
-        p.map(
-            partial(
-                self.dummy_worker, 
-                some_q=q), 
-            range(10)
-        )
+        p.map(partial(self.dummy_worker, some_q=q), range(10))
 
         # merge
         print("job finished!")
