003f5b7e14e3f260c2ff9c20ebf81b1cdc9e2eb1
gaojx <gaojxcs@gmail.com>
2019-03-21 11:11:06 +0800

    simplify function names

--- test_pipeline.py
+++ test_pipeline.py
@@ -3,11 +3,11 @@ import unittest
 
 class TestPipeline(unittest.TestCase):
     def test_compute_ner_for_clinical_trails(self):
-        from pipeline import compute_ner_for_clinical_trails
+        from pipeline import load_compute_ner
         NUM_OF_ROWS = 2
-        row_generator = compute_ner_for_clinical_trails(NUM_OF_ROWS, "nct_id")
+        row_generator = load_compute_ner(NUM_OF_ROWS, "nct_id")
         row_num = 0
-        for row in row_generator:
+        for _ in row_generator:
             # pprint(row)
             row_num += 1
         self.assertEqual(row_num, NUM_OF_ROWS)
