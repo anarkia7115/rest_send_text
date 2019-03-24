64a98c453459cf793b8b2a01021ba3bb6723400c
gaojx <gaojxcs@gmail.com>
2019-03-24 20:55:50 +0800

    clear the pipe bash

---
+++ clear_the_pipe.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+for i in {1..100}
+do
+    curl -X POST 'http://localhost:5000/annotate/text' \
+    --data '{"text": "The phosphorylation of Hdm2 by MK2 promotes the ubiquitination of p53."}'
+done
