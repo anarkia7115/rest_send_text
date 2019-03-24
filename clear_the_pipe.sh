bdb21b6fb91a207b4b90f3a1c3b53a7f4954bb30
gaojx <gaojxcs@gmail.com>
2019-03-24 20:58:40 +0800

    fix host:port variables

--- clear_the_pipe.sh
+++ clear_the_pipe.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 
+host="localhost"
+port="5000"
 for i in {1..100}
 do
-    curl -X POST 'http://localhost:5000/annotate/text' \
+    curl -X POST "http://${host}:${port}/annotate/text" \
     --data '{"text": "The phosphorylation of Hdm2 by MK2 promotes the ubiquitination of p53."}'
 done
