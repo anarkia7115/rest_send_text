5e357054e178b2915b81404c68ce72a54a003109
gaojx <gaojxcs@gmail.com>
2019-03-25 09:10:31 +0800

    load and save row json file

---
+++ data_helpers.py
@@ -0,0 +1,13 @@
+import json
+def dict_save(row_generator, output_file):
+    with open(output_file, 'w') as fw:
+        for row in row_generator:
+            row_json = json.dumps(row)
+            fw.write(row_json+"\n")
+
+
+def dict_load(row_records_file):
+    with open(row_records_file) as f_in:
+        for row in f_in:
+            row_json = json.dumps(row)
+            yield row_json
