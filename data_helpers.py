63a33cca4d5eaf1c339af5df31a82667e8a7cf70
gaojx <gaojxcs@gmail.com>
2019-03-25 09:17:36 +0800

    fix json loader

--- data_helpers.py
+++ data_helpers.py
@@ -9,5 +9,5 @@ def dict_save(row_generator, output_file):
 def dict_load(row_records_file):
     with open(row_records_file) as f_in:
         for row in f_in:
-            row_json = json.dumps(row)
+            row_json = json.loads(row)
             yield row_json
